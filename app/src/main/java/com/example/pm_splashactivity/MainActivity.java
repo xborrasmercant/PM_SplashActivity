95305604f2bf2a9f538ca5ff3df3788bde8095fc
Xavier <91749310+arseniox@users.noreply.github.com>
2023-12-01 19:07:57 +0100

    Finished

--- app/src/main/java/com/example/pm_splashactivity/MainActivity.java
+++ app/src/main/java/com/example/pm_splashactivity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.pm_splashactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Animation spinAnim = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
+
+
+        LayoutAnimationController controller =
+                new LayoutAnimationController(spinAnim);
+        TableLayout table = findViewById(R.id.splashTable);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.setLayoutAnimation(controller);
+        }
     }
 }
